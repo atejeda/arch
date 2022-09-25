3f7278119e99222b732111dbad4a79c09678d32f
Alexis Tejeda <alexis.tejeda@gmail.com>
2022-09-25 12:14:17 -0300

    Adds scrip to update arch

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -12,6 +12,7 @@ import XMonad.Util.SpawnOnce
 import XMonad.Hooks.SetWMName
 import XMonad.Layout.MultiColumns
 import XMonad.Layout.Spacing
+import XMonad.Layout.LayoutModifier
 import XMonad.Hooks.DynamicLog (dynamicLogWithPP, xmobarPP, xmobarColor, shorten, PP(..))
 import XMonad.Hooks.ManageDocks (avoidStruts, manageDocks, docksEventHook)
 
@@ -19,6 +20,8 @@ import Graphics.X11.ExtraTypes.XF86
 
 import Data.Monoid
 import System.Exit
+
+import Data.List as List
 
 import qualified XMonad.StackSet as W
 import qualified Data.Map        as M
@@ -199,6 +202,9 @@ myMouseBindings (XConfig {XMonad.modMask = modm}) = M.fromList $
 ------------------------------------------------------------------------
 -- Layouts:
 
+mySpacing :: Integer -> l a -> XMonad.Layout.LayoutModifier.ModifiedLayout Spacing l a
+mySpacing i = spacingRaw False (Border i i i i) True (Border i i i i) True
+
 -- You can specify and transform your layouts by modifying these values.
 -- If you change layout bindings be sure to use 'mod-shift-space' after
 -- restarting (with 'mod-q') to reset your layout state to the new
@@ -326,52 +332,61 @@ defaults = def {
 
 -- | Finally, a copy of the default bindings in simple textual tabular format.
 help :: String
-help = unlines ["The default modifier key is 'alt'. Default keybindings:",
-    "",
-    "-- launching and killing programs",
-    "mod-Shift-Enter  Launch xterminal",
-    "mod-p            Launch dmenu",
-    "mod-Shift-p      Launch gmrun",
-    "mod-Shift-c      Close/kill the focused window",
-    "mod-Space        Rotate through the available layout algorithms",
-    "mod-Shift-Space  Reset the layouts on the current workSpace to default",
-    "mod-n            Resize/refresh viewed windows to the correct size",
-    "mod-Shift-/      Show this help message with the default keybindings",
-    "",
-    "-- move focus up or down the window stack",
-    "mod-Tab        Move focus to the next window",
-    "mod-Shift-Tab  Move focus to the previous window",
-    "mod-j          Move focus to the next window",
-    "mod-k          Move focus to the previous window",
-    "mod-m          Move focus to the master window",
-    "",
-    "-- modifying the window order",
-    "mod-Return   Swap the focused window and the master window",
-    "mod-Shift-j  Swap the focused window with the next window",
-    "mod-Shift-k  Swap the focused window with the previous window",
-    "",
-    "-- resizing the master/slave ratio",
-    "mod-h  Shrink the master area",
-    "mod-l  Expand the master area",
-    "",
-    "-- floating layer support",
-    "mod-t  Push window back into tiling; unfloat and re-tile it",
-    "",
-    "-- increase or decrease number of windows in the master area",
-    "mod-comma  (mod-,)   Increment the number of windows in the master area",
-    "mod-period (mod-.)   Deincrement the number of windows in the master area",
-    "",
-    "-- quit, or restart",
-    "mod-Shift-q  Quit xmonad",
-    "mod-q        Restart xmonad",
-    "mod-[1..9]   Switch to workSpace N",
-    "",
-    "-- Workspaces & screens",
-    "mod-Shift-[1..9]   Move client to workspace N",
-    "mod-{w,e,r}        Switch to physical/Xinerama screens 1, 2, or 3",
-    "mod-Shift-{w,e,r}  Move client to screen 1, 2, or 3",
-    "",
-    "-- Mouse bindings: default actions bound to mouse events",
-    "mod-button1  Set the window to floating mode and move by dragging",
-    "mod-button2  Raise the window to the top of the stack",
-    "mod-button3  Set the window to floating mode and resize by dragging"]
+-- help = unlines ["The default modifier key is 'alt'. Default keybindings:",
+--     "",
+--     "-- launching and killing programs",
+--     "mod-Shift-Enter  Launch xterminal",
+--     "mod-p            Launch dmenu",
+--     "mod-Shift-p      Launch gmrun",
+--     "mod-Shift-c      Close/kill the focused window",
+--     "mod-Space        Rotate through the available layout algorithms",
+--     "mod-Shift-Space  Reset the layouts on the current workSpace to default",
+--     "mod-n            Resize/refresh viewed windows to the correct size",
+--     "mod-Shift-/      Show this help message with the default keybindings",
+--     "",
+--     "-- move focus up or down the window stack",
+--     "mod-Tab        Move focus to the next window",
+--     "mod-Shift-Tab  Move focus to the previous window",
+--     "mod-j          Move focus to the next window",
+--     "mod-k          Move focus to the previous window",
+--     "mod-m          Move focus to the master window",
+--     "",
+--     "-- modifying the window order",
+--     "mod-Return   Swap the focused window and the master window",
+--     "mod-Shift-j  Swap the focused window with the next window",
+--     "mod-Shift-k  Swap the focused window with the previous window",
+--     "",
+--     "-- resizing the master/slave ratio",
+--     "mod-h  Shrink the master area",
+--     "mod-l  Expand the master area",
+--     "",
+--     "-- floating layer support",
+--     "mod-t  Push window back into tiling; unfloat and re-tile it",
+--     "",
+--     "-- increase or decrease number of windows in the master area",
+--     "mod-comma  (mod-,)   Increment the number of windows in the master area",
+--     "mod-period (mod-.)   Deincrement the number of windows in the master area",
+--     "",
+--     "-- quit, or restart",
+--     "mod-Shift-q  Quit xmonad",
+--     "mod-q        Restart xmonad",
+--     "mod-[1..9]   Switch to workSpace N",
+--     "",
+--     "-- Workspaces & screens",
+--     "mod-Shift-[1..9]   Move client to workspace N",
+--     "mod-{w,e,r}        Switch to physical/Xinerama screens 1, 2, or 3",
+--     "mod-Shift-{w,e,r}  Move client to screen 1, 2, or 3",
+--     "",
+--     "-- Mouse bindings: default actions bound to mouse events",
+--     "mod-button1  Set the window to floating mode and move by dragging",
+--     "mod-button2  Raise the window to the top of the stack",
+--     "mod-button3  Set the window to floating mode and resize by dragging"]
+
+help = unlines [ "something " ]
+
+
+
+-- showMap :: (Show k, Show v) => M.Map k v -> String
+-- showMap = List.intercalate "\n" . map show . M.toList
+
+-- help = showMap myKeys
---
+++ update.arch.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+pacman -S archlinux-keyring
+pacman -Syyu
